459c57cb867f9479db89ec6f8bcdf4da10b73542
yusufbadawi <yusuf_badawi@hotmail.com>
2022-06-14 21:12:36 +0200

    payment a5eraan

--- pages/payment.js
+++ pages/payment.js
@@ -12,7 +12,7 @@ const imageUrls = [
   "https://logos-world.net/wp-content/uploads/2020/09/Mastercard-Logo.png",
 ];
 
-export default function App() {
+const App = () => {
   const [creditCardNum, setCreditCardNum] = useState("");
   const [cardType, setCardType] = useState("");
   const [cardHolder, setCardHolder] = useState("");
@@ -30,6 +30,11 @@ export default function App() {
   // const [expireMonthState, setExpireMonthState] = useState("January");
   // const [expireYearState, setExpireYearState] = useState("2021");
 
+  const sleep = ms => new Promise(
+    resolve => setTimeout(resolve, ms)
+  );
+  
+
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -37,7 +42,7 @@ export default function App() {
 
   const validateEmail = (value) => {
     const emailRegex =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     let emailState;
     if (emailRegex.test(value)) {
       emailState = "has-success";
@@ -51,7 +56,7 @@ export default function App() {
 
   const validateAddress = (value) => {
     let addressState;
-    if (value?.length  > 2) {
+    if (value?.length > 2) {
       addressState = "has-success";
       console.log("Success");
     } else {
@@ -126,14 +131,14 @@ export default function App() {
   // }
 
   const handleEmail = (e) => {
-    validateEmail(e.target.rawValue);
-    setEmail(e.target.rawValue);
+    validateEmail(e.target.value);
+    setEmail(e.target.value);
     console.log(e.target.value);
   };
 
   const handleAddress = (e) => {
-    validateAddress(e.target.rawValue);
-    setAddress(e.target.rawValue);
+    validateAddress(e.target.value);
+    setAddress(e.target.value);
     console.log(e.target.value);
   };
 
@@ -220,144 +225,170 @@ export default function App() {
     }
   };
 
+
+
   return (
-    <Modal show={true} onHide={handleClose}>
-      <Modal.Header> Checkout </Modal.Header>
-      <Modal.Body>
-        <div class="col-md-12 text-center">
-          <Form className={Styles.container}>
-            <form id="form">
-              <div className="input-grp">
-                <div class="input-container">
-                  <h1> Personal Details </h1>
-                </div>
-              </div>
+    <>
+      {/* <div id="formTest">
+        <Button
+          className="order"
+          variant="dark"
+          type="submit"
+          onClick={handleShow}
+        >
+          Checkout
+        </Button>
+      </div> */}
 
-              <div>
-                <input
-                  name="email"
-                  value={email}
-                  valid={emailState === "has-success"}
-                  invalid={emailState === "has-danger"}
-                  onChange={handleEmail}
-                  type="text"
-                  placeholder="Email"
-                  required
-                />
-              </div>
+      
+    
 
-              <div>
-                <input
-                  name="address"
-                  value={address}
-                  valid={addressState === "has-success"}
-                  invalid={addressState === "has-danger"}
-                  onChange={handleAddress}
-                  type="text"
-                  placeholder="Address"
-                  required
-                />
-              </div>
+      <Modal show={true} onHide={handleClose}>
+        <Modal.Header> Checkout </Modal.Header>
+        <Modal.Body>
+          <div class="col-md-12 text-center">
+            <Form id="form" className={Styles.container}>
+              <div className="container">
+                <form>
+                  <div className="input-grp">
+                    <div class="input-container">
+                      <h1> Personal Details </h1>
+                    </div>
+                  </div>
+                  <div>
+                    <input
+                      name="email"
+                      value={email}
+                      valid={emailState === "has-success"}
+                      invalid={emailState === "has-danger"}
+                      onChange={handleEmail}
+                      type="text"
+                      placeholder="Email"
+                      required
+                    />
+                  </div>
 
-              <div className="input-grp">
-                <div class="input-container">
-                  <h1> Credit Card </h1>
-                </div>
-                <div id="visa" class="input-container">
-                  <img className="logo" src={imageUrls[0]} alt="Card logo" />
-                </div>
-                <div id="mastercard" class="input-container">
-                  <img className="logo" src={imageUrls[1]} alt="Card logo" />
-                </div>
-              </div>
+                  <div>
+                    <input
+                      name="address"
+                      value={address}
+                      valid={addressState === "has-success"}
+                      invalid={addressState === "has-danger"}
+                      onChange={handleAddress}
+                      type="text"
+                      placeholder="Address"
+                      required
+                    />
+                  </div>
 
-              <div>
-                <Cleave
-                  name="cardNumber"
-                  delimiter="-"
-                  options={{
-                    creditCard: true,
-                  }}
-                  value={creditCardNum}
-                  valid={creditCardNumState === "has-success"}
-                  invalid={creditCardNumState === "has-danger"}
-                  onChange={handleNum}
-                  placeholder="Card Number"
-                />
-                <FormFeedback>
-                  ID is incorrect or has been registered with before.
-                </FormFeedback>
-              </div>
+                  <div className="input-grp">
+                    <div class="input-container">
+                      <h1> Credit Card </h1>
+                    </div>
+                    <div id="visa" class="input-container">
+                      <img
+                        className="logo"
+                        src={imageUrls[0]}
+                        alt="Card logo"
+                      />
+                    </div>
+                    <div id="mastercard" class="input-container">
+                      <img
+                        className="logo"
+                        src={imageUrls[1]}
+                        alt="Card logo"
+                      />
+                    </div>
+                  </div>
 
-              <div>
-                <input
-                  name="cardHolder"
-                  value={cardHolder}
-                  valid={cardHolderState === "has-success"}
-                  invalid={cardHolderState === "has-danger"}
-                  onChange={handleCardHolder}
-                  type="text"
-                  placeholder="Card Holder Name"
-                  required
-                />
-              </div>
+                  <div>
+                    <Cleave
+                      name="cardNumber"
+                      delimiter="-"
+                      options={{
+                        creditCard: true,
+                      }}
+                      value={creditCardNum}
+                      valid={creditCardNumState === "has-success"}
+                      invalid={creditCardNumState === "has-danger"}
+                      onChange={handleNum}
+                      placeholder="Card Number"
+                    />
+                  </div>
 
-              <div className="input-grp">
-                <div className="input-container">
-                  <h4>Exp. Month</h4>
-                  <select value={expireMonth} onChange={handleExpMonth}>
-                    <option value="January">January</option>
-                    <option value="February">February</option>
-                    <option value="March">March</option>
-                    <option value="April">April</option>
-                    <option value="May">May</option>
-                    <option value="June">June</option>
-                    <option value="July">July</option>
-                    <option value="August">August</option>
-                    <option value="September">September</option>
-                    <option value="October">October</option>
-                    <option value="November">November</option>
-                    <option value="December">December</option>
-                  </select>
-                </div>
-                <div className="input-container">
-                  <h4>Exp. Year</h4>
-                  <select value={expireYear} onChange={handleExpYear}>
-                    <option value="2023">2023</option>
-                    <option value="2024">2024</option>
-                    <option value="2025">2025</option>
-                    <option value="2026">2026</option>
-                    <option value="2027">2027</option>
-                    <option value="2028">2028</option>
-                    <option value="2029">2029</option>
-                    <option value="2029">2030</option>
-                    <option value="2029">2031</option>
-                  </select>
-                </div>
-                <div className="input-container">
-                  <h4>CVV</h4>
-                  <input
-                    name="cvv"
-                    type="password"
-                    placeholder="CVV"
-                    value={cvv}
-                    valid={cvvState === "has-success"}
-                    invalid={cvvState === "has-danger"}
-                    onChange={handleCVV}
-                    maxLength={3}
-                    required
-                  />
-                </div>
-              </div>
+                  <div>
+                    <input
+                      name="cardHolder"
+                      value={cardHolder}
+                      valid={cardHolderState === "has-success"}
+                      invalid={cardHolderState === "has-danger"}
+                      onChange={handleCardHolder}
+                      type="text"
+                      placeholder="Card Holder Name"
+                      required
+                    />
+                  </div>
 
-              <Button
-                onClick={handleSubmit}
-                class="btn btn-primary btn-lg"
-              >{`${cardType} Pay `}</Button>
-            </form>
-          </Form>
-        </div>
-      </Modal.Body>
-    </Modal>
+                  <div className="input-grp">
+                    <div className="input-container">
+                      <h4>Exp. Month</h4>
+                      <select value={expireMonth} onChange={handleExpMonth}>
+                        <option value="January">January</option>
+                        <option value="February">February</option>
+                        <option value="March">March</option>
+                        <option value="April">April</option>
+                        <option value="May">May</option>
+                        <option value="June">June</option>
+                        <option value="July">July</option>
+                        <option value="August">August</option>
+                        <option value="September">September</option>
+                        <option value="October">October</option>
+                        <option value="November">November</option>
+                        <option value="December">December</option>
+                      </select>
+                    </div>
+                    <div className="input-container">
+                      <h4>Exp. Year</h4>
+                      <select value={expireYear} onChange={handleExpYear}>
+                        <option value="2023">2023</option>
+                        <option value="2024">2024</option>
+                        <option value="2025">2025</option>
+                        <option value="2026">2026</option>
+                        <option value="2027">2027</option>
+                        <option value="2028">2028</option>
+                        <option value="2029">2029</option>
+                        <option value="2029">2030</option>
+                        <option value="2029">2031</option>
+                      </select>
+                    </div>
+                    <div className="input-container">
+                      <h4>CVV</h4>
+                      <input
+                        name="cvv"
+                        type="password"
+                        placeholder="CVV"
+                        value={cvv}
+                        valid={cvvState === "has-success"}
+                        invalid={cvvState === "has-danger"}
+                        onChange={handleCVV}
+                        maxLength={3}
+                        required
+                      />
+                    </div>
+                  </div>
+
+                  <Button
+                    onClick={handleSubmit}
+                    class="btn btn-primary btn-lg"
+                  >{`${cardType} Pay `}</Button>
+                </form>
+              </div>
+            </Form>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </>
   );
-}
+};
+
+export default App;
